a49abaa2396e0e94142045c411fcfc0c6f74646c
Nguyễn Quốc Hiệu <121285650+nqh-tq-32052503@users.noreply.github.com>
2026-01-02 19:42:09 +0700

    Update: add original ViT

--- models/fft.py
+++ models/fft.py
@@ -123,7 +123,7 @@ class ViT_lora_fft(VisionTransformer):
 def _create_vision_transformer(variant='vit_base_patch16_224.augreg_in21k_ft_in1k', pretrained=False, **kwargs):
     import timm
     pretrained_vit = timm.create_model(variant, pretrained=True)
-    model = QuantizeViT() # VisionTransformer() # ViT_lora_fft()
+    model = VisionTransformer() # QuantizeViT() # VisionTransformer() # ViT_lora_fft()
     pretrained_state = pretrained_vit.state_dict()
     incompatible = model.load_state_dict(pretrained_state, strict=False)
 
